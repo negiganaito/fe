517b0e29f7a667fef8df6b2af0046ee34cb38663
Lê Xuân Tiến <tienlx97@gmail.com>
2023-12-25 17:00:24 +0700

    chore: popover

--- src/faang/popover/index.js
+++ src/faang/popover/index.js
@@ -5,29 +5,16 @@
  * See the LICENSE file in the root directory for details.
  */
 export {} from "./base-multi-page-view";
-
 export {} from "./base-multi-page-view-container";
-
 export {} from "./base-multi-page-view-reducer";
-
 export {} from "./base-popover";
-
 export {} from "./base-popover-down-edge-arrow";
-
 export {} from "./base-popover-down-inset-arrow";
-
 export {} from "./base-popover-right-edge-arrow";
-
 export {} from "./base-popover-right-inset-arrow";
-
 export {} from "./base-popover-svg-arrow-container";
-
 export {} from "./base-scrollable-area";
-
 export {} from "./comet-form-combobox-menu-item";
-
 export {} from "./comet-list-cell-strict";
-
 export {} from "./comet-menu-base";
-
 export {} from "./comet-menu-base";
