f3b28cfd2c1f87d6c18e6086bf845fc19cc41a07
Lê Xuân Tiến <tienlx97@gmail.com>
2024-01-16 16:53:46 +0700

    chore: 1

--- be/mutation.js
+++ be/mutation.js
@@ -1,6 +1,6 @@
-const { GraphQLObjectType, GraphQLNonNull, GraphQLString } = require('graphql')
+const { GraphQLObjectType,GraphQLInputObjectType, GraphQLNonNull, GraphQLString } = require('graphql')
 
-const WorkGalahadVariantInputType = new GraphQLObjectType({
+const WorkGalahadVariantInput = new GraphQLInputObjectType({
   name: 'WorkGalahadVariantInput',
   fields: {
     variant: {
@@ -12,15 +12,35 @@ const WorkGalahadVariantInputType = new GraphQLObjectType({
   },
 })
 
- const MutationType = new GraphQLObjectType({
+const DisableWorkGalahadVariantV2ResponsePayload = new GraphQLObjectType({
+  name:"DisableWorkGalahadVariantV2ResponsePayload",
+  fields: {
+    __typename: {
+      type: new GraphQLNonNull(GraphQLString),
+    }
+  },
+})
+
+const EnableWorkGalahadVariantV2ResponsePayload = new GraphQLObjectType({
+  name:"EnableWorkGalahadVariantV2ResponsePayload",
+  fields: {
+    __typename: {
+      type: new GraphQLNonNull(GraphQLString),
+    }
+  },
+})
+
+
+const MutationType = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    WorkGalahadVariantEnableMutation: {
+    enable_work_galahad_variant_v2: {
       args: {
         input: {
-          type: WorkGalahadVariantInputType,
+          type: WorkGalahadVariantInput,
         },
       },
+      type: EnableWorkGalahadVariantV2ResponsePayload,
       resolve: () => {
         return {
           __typeName: 'EnableWorkGalahadVariantV2ResponsePayload',
@@ -28,12 +48,13 @@ const WorkGalahadVariantInputType = new GraphQLObjectType({
       },
     },
 
-    WorkGalahadVariantDisableMutation: {
+    disable_work_galahad_variant_v2: {
       args: {
         input: {
-          type: WorkGalahadVariantInputType,
+          type: WorkGalahadVariantInput,
         },
       },
+      type: DisableWorkGalahadVariantV2ResponsePayload,
       resolve: () => {
         return {
           __typeName: 'DisableWorkGalahadVariantV2ResponsePayload',
